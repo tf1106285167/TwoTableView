491d405b06a96ae5db83c14ad7199502ff31111c
tf1106285167 <1106285167@qq.com>
2016-07-05 17:16:50 +0800

    Initial Commit

---
+++ TwoTableView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TwoTableView
+//
+//  Created by TuFa on 16/7/5.
+//  Copyright © 2016年 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TwoTableView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TwoTableView
+//
+//  Created by TuFa on 16/7/5.
+//  Copyright © 2016年 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+

79fee441a34d2bce2f9b9d0f1248a9177ba478b9
tf1106285167 <1106285167@qq.com>
2016-07-06 17:25:23 +0800

    实现在一个控制器中操作两个TableView
    实现在一个控制器中对两个TableView进行操作

--- TwoTableView/ViewController.m
+++ TwoTableView/ViewController.m
@@ -8,20 +8,100 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
 
+#define KSCreenWith [UIScreen mainScreen].bounds.size.width
+#define KSCreenHeight [UIScreen mainScreen].bounds.size.height
+
+@interface ViewController ()<UITableViewDelegate,UITableViewDataSource>
+{
+    UITableView *tableV1;
+    UITableView *tableV2;
+    
+    NSInteger tableV2_row;
+}
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    [self creatTableView];
+    
+    //选中tableV1的第一行
+    [self tableView:tableV1 didSelectRowAtIndexPath:[NSIndexPath indexPathForRow:0 inSection:0]];
+    [tableV1 selectRowAtIndexPath:[NSIndexPath indexPathForRow:0 inSection:0] animated:YES scrollPosition:UITableViewScrollPositionTop];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+-(void)creatTableView{
+    
+    tableV1 = [[UITableView alloc]initWithFrame:CGRectMake(0, 20, KSCreenWith/2, KSCreenHeight-20) style:UITableViewStylePlain];
+    tableV1.delegate = self;
+    tableV1.dataSource = self;
+    [self.view addSubview:tableV1];
+    
+    tableV2 = [[UITableView alloc]initWithFrame:CGRectMake(KSCreenWith/2, 20, KSCreenWith/2, KSCreenHeight-20) style:UITableViewStylePlain];
+    tableV2.backgroundColor = [UIColor orangeColor];
+    tableV2.delegate = self;
+    tableV2.dataSource = self;
+    [self.view addSubview:tableV2];
+    
 }
+
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+
+    if (tableView == tableV1) {
+        
+        return 20;
+    }else{
+        return tableV2_row;
+    }
+    
+    return 0;
+}
+
+-(UITableViewCell*)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    
+    //表1
+    if (tableView == tableV1) {
+        
+        static NSString *cellId = @"identify";
+        UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellId];
+        
+        if (!cell) {
+            
+            cell = [[UITableViewCell alloc]initWithStyle:UITableViewCellStyleDefault reuseIdentifier:cellId];
+        }
+        cell.textLabel.text = [NSString stringWithFormat:@"表1_第%ld行",(long)indexPath.row];
+    return cell;
+    }
+    
+    //表2
+    static NSString *cellId2 = @"cell";
+    UITableViewCell *cell2 = [tableView dequeueReusableCellWithIdentifier:cellId2];
+    
+    if (!cell2) {
+        
+        cell2 = [[UITableViewCell alloc]initWithStyle:UITableViewCellStyleDefault reuseIdentifier:cellId2];
+    }
+    cell2.backgroundColor = [UIColor orangeColor];
+    cell2.textLabel.text = [NSString stringWithFormat:@"表2_第%ld行",(long)indexPath.row];
+    return cell2;
+}
+
+-(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
+    
+    tableV2_row = 1+arc4random_uniform(15);//随机数1-15行
+    [tableV2 reloadData];
+}
+
 
 @end
